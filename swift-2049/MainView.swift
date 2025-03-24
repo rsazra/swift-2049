4dfd83fee8e89aafea3d226cb3354d2029cd2053
rsazra <rajbir.singh.azra@gmail.com>
2025-03-24 19:23:30 -0400

    initial setup + working score interop between uikit and swift ui

---
+++ swift-2049/MainView.swift
@@ -0,0 +1,72 @@
+//
+//  MainView.swift
+//  swift-2049
+//
+//  Created by Rajbir Singh Azra on 2025-03-24.
+//
+
+import SwiftUI
+import UIKit
+
+struct SwiftUINumberTileController: UIViewControllerRepresentable {
+    @Binding var score: Int?
+    
+    func makeUIViewController(context: Context) -> NumberTileGameViewController {
+        let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
+        let originalDelegate = game.model?.delegate
+        context.coordinator.originalDelegate = originalDelegate!
+        game.model?.delegate = context.coordinator
+        return game
+    }
+    
+    func updateUIViewController(_ uiViewController: NumberTileGameViewController, context: Context) {
+//        score = uiViewController.model?.score
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    
+    class Coordinator: NSObject, GameModelProtocol {
+        func moveOneTile(from: (Int, Int), to: (Int, Int), value: Int) {
+            originalDelegate?.moveOneTile(from: from, to: to, value: value)
+        }
+        
+        func moveTwoTiles(from: ((Int, Int), (Int, Int)), to: (Int, Int), value: Int) {
+            originalDelegate?.moveTwoTiles(from: from, to: to, value: value)
+        }
+        
+        func insertTile(at location: (Int, Int), withValue value: Int) {
+            originalDelegate?.insertTile(at: location, withValue: value)
+        }
+        
+        var parent: SwiftUINumberTileController
+        var originalDelegate: GameModelProtocol?
+        
+        init(_ parent: SwiftUINumberTileController) {
+            self.parent = parent
+        }
+        
+        func scoreChanged(to newScore: Int) {
+            originalDelegate?.scoreChanged(to: newScore)
+            DispatchQueue.main.async {
+                self.parent.score = newScore
+            }
+        }
+    }
+}
+
+struct MainView: View {
+    @State private var score: Int? = 0
+    var body: some View {
+        VStack {
+            Text("Score: \(score ?? 1)")
+            SwiftUINumberTileController(score: $score)
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
